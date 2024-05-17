00a8cf574990a0e7026c7fcc51b7bbe8c2a3451c
Emilse Erazo <emilsesilvana@gmail.com>
2024-05-16 21:37:47 -0300

    PrimerPreEntregaErazoReac

---
+++ coder-react/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='Titulo'>ZINNIA</h1>
+        <div>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+            <li>Contacto</li>
+
+        </div>
+    </>
+  )
+}
+
+export default App
